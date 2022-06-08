5dfa3f4b67d7af4721bd839ee785b1f34f20f43e
Melanie0522 <106500812+Melanie0522@users.noreply.github.com>
2022-06-08 15:47:47 +0800

    Add files via upload

---
+++ js_object.js
@@ -0,0 +1,15 @@
+/* 1 */
+var user={
+name: John;
+surname: Mike
+};
+user.name=Mike;
+delete user.name;
+
+/* 2 */
+var fruit = {
+apple: 20,
+pear: 20,
+peach: 10
+};
+document.write(fruit.apple+fruit.pear+fruit.peach);
